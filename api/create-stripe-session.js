8866ef9f0ccbc9a42f5515387f65f128c19b157a
vesaone <codypeisley95@hotmail.com>
2025-04-23 15:32:29 +1000

    84

--- api/create-stripe-session.js
+++ api/create-stripe-session.js
@@ -1,4 +1,8 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-08-16',
+});
+
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
